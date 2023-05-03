10b6efe6ee2381400f4dc06ea2d7be1ed67b7d2b
Bryant Wiggins <topwigit@gmail.com>
2023-05-02 21:49:20 -0400

    done!

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+const drivers1 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(char =>{
+        return char.charAt() === string.charAt()
+    })
+}
+
+
+function matchName(drivers, string){
+    return drivers.filter(obj => {
+        return obj.name === string
+    })
+}
